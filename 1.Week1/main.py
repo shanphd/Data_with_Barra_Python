0cdce079b41656c4740e344ed4fa6d60d068b982
Shanphd <klgowrishankarphd@gmail.com>
2025-06-18 20:54:01 +0530

    Added week2 data

---
+++ 1.Week1/main.py
@@ -0,0 +1,12 @@
+# /// script
+# requires-python = ">=3.13"
+# dependencies = []
+# ///
+
+import pygame
+def main() -> None:
+    print("Hello from main.py!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from data-with-barra-python!")
+
+
+if __name__ == "__main__":
+    main()
